e67aaecd4fdfb45a4afda042138344a500f2533a
euguroglu <e.uguroglu@yahoo.com>
2021-05-16 16:35:01 +0300

    code templated adjusted

--- StreamingWC.py
+++ StreamingWC.py
@@ -2,14 +2,21 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import expr
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("Streaming Word Count").config("spark.streaming.stopGracefullyOnShutdown", "true").config("spark.sql.shuffle.partitions", 3).getOrCreate()
+    spark = SparkSession \
+        .builder.appName("Streaming Word Count") \
+        .config("spark.streaming.stopGracefullyOnShutdown", "true") \
+        .config("local[*]") \
+        .getOrCreate()
 
 ## Read
 ## Reading data from port 9999
-    lines_df = spark.readStream.format("socket").option("host", "localhost").option("port", "9999").load()
+    lines_df = spark.readStream \
+        .format("socket") \
+        .option("host", "localhost") \
+        .option("port", "9999") \
+        .load()
 
     #lines_df.printSchema()
-
 
 ## Transform
 ## Transform it and save it as dataframe with column name word and apply count method
@@ -18,7 +25,10 @@ if __name__ == "__main__":
 
 ## Sink
 ## Sink it into console
-    word_count_query = counts_df.writeStream.format("console").option("checkpointLocation", "chk-point-dir").outputMode("complete").start()
-
+    word_count_query = counts_df.writeStream. \
+        format("console") \
+        .option("checkpointLocation", "chk-point-dir") \
+        .outputMode("complete") \
+        .start()
 
     word_count_query.awaitTermination()
